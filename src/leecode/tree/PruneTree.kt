07c56e053185c456be5b0ffded3f027c0751cd3a
qiang <zengqiang11111@gmail.com>
2022-06-21 23:39:20 +0800

    Add Tree

---
+++ src/leecode/tree/PruneTree.kt
@@ -0,0 +1,29 @@
+package leecode.tree
+
+import tree.TreeNode
+//利用后序遍历，递归解决二叉树剪枝。
+class PruneTree {
+    /**
+     * Example:
+     * var ti = TreeNode(5)
+     * var v = ti.`val`
+     * Definition for a binary tree node.
+     * class TreeNode(var `val`: Int) {
+     *     var left: TreeNode? = null
+     *     var right: TreeNode? = null
+     * }
+     */
+    fun pruneTree(root: TreeNode?): TreeNode? {
+        if (root == null) { // 定义终止条件，叶节点时退递归
+            return root
+        }
+        //明确调用单元： 左子树为空，右子树为空，当前为0，则剪枝。
+        root?.left = pruneTree(root.left)
+        root?.right = pruneTree(root.right)
+        if (root?.`val` == 0 && root?.left == null && root?.right == null) {
+            return null
+        } else {
+            return root  //返回剪辑后的节点
+        }
+    }
+}
--- src/tree/TreeNode.java
+++ src/tree/TreeNode.java
@@ -1,9 +1,9 @@
 package tree;
 
 public class TreeNode {
-    int val = 0;
-    TreeNode left;
-    TreeNode right;
+    public int val = 0;
+    public TreeNode left;
+    public TreeNode right;
 
     public TreeNode(int val){
         this.val = val;
